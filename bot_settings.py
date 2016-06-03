071681bc91731f6a63004ad34d812c3b44722923
fatema <fatemabox@gmail.com>
2016-06-03 23:51:42 +0000

    Changes name to eleanor and fixes email

--- bot_settings.py
+++ bot_settings.py
@@ -2,7 +2,7 @@
 
 # IRC configuration
 channel = "#falconframework"
-botnick = "falconbot"
+botnick = "Elaenor"
 server = "irc.freenode.net"
 registered = False
 
@@ -14,5 +14,7 @@ nick_source = "nicks.csv"
 # Bot text
 hello_list = ["hello", "hi", "hey", "yo", "sup"]
 help_list = ["help", "info", "faq", "explain_yourself"]
-welcome_message = "Welcome! The channel is pretty quiet right now, so I thought I'd say hello"", and ping some people that you're here (like kgriffs). I'm a bot! If no one responds for"
-" a whle, try emailing us at falcon@librelist.com or just try coming back later." 
+welcome_message = ("Welcome! The channel is pretty quiet right now, so I thought I'd "
+"say hello, and ping some people that you're here (like kgriffs). I'm a bot! If no one "
+"responds for a whle, try emailing us at users@mail.falconframework.org or just try "
+"coming back later.")
