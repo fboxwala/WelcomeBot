00e7eabe6791a22673f8bb3eb0ee1a586e0ef8a4
Fatema Boxwala <fatemabox@gmail.com>
2016-06-10 05:56:47 -0500

    adds jvrbanac to greeters list

--- bot_settings.py
+++ bot_settings.py
@@ -8,13 +8,19 @@ registered = False
 
 # Bot behavior
 wait_time = 60
-channel_greeters = ["kgriffs"]
+channel_greeters = ["kgriffs", "jvrbanac"]
 nick_source = "nicks.csv"
 
 # Bot text
 hello_list = ["hello", "hi", "hey", "yo", "sup"]
 help_list = ["help", "info", "faq", "explain_yourself"]
-welcome_message = ("Welcome! The channel is pretty quiet right now, so I thought I'd "
-"say hello, and ping some people that you're here (like kgriffs). I'm a bot! If no one "
-"responds for a whle, try emailing us at users@mail.falconframework.org or just try "
-"coming back later.")
+welcome_message = ("Welcome! The channel is pretty quiet "
+                   "right now, so I thought I'd say "
+                   "hello, and ping some people that "
+                   "you're here (like " +
+                   ' '.join(channel_greeters) +
+                   "). "
+                   "I'm a bot! If no one responds for "
+                   "a while, try emailing us at "
+                   "users@mail.falconframework.org or just try "
+                   "coming back later.")
